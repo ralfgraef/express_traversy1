030be25aeb4829b2db52b003b589053624883e25
Ralf Gräf <ralfgraef@Ralfs-MacBook-Pro.local>
2019-01-24 00:01:26 +0100

    add middlewares

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ const path = require('path');
 
 const app = express();
 
+/* const logger = (res, req, next) => {
+	console.log('Logging...');
+	next();
+}
+
+app.use(logger); */
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set static path
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-	res.send('Hello World from Ralf');
+	res.send('Hello from Ralf');
 });
 
 app.listen(3000, () => {
